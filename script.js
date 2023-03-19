d6653e179a516b7aabd93b9e558e155119e75149
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-19 10:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const calculateTotalBtn = document.getElementById('calculate-total-btn');
-		calculateTotalBtn.addEventListener('click', calculateTotal);
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
 
-		function calculateTotal() {
-			const prices = document.querySelectorAll('.price');
-			let totalPrice = 0;
-			prices.forEach(price => {
-				totalPrice += parseFloat(price.textContent);
-			});
-			const totalPriceCell = document.getElementById('ans');
-			totalPriceCell.textContent = totalPrice.toFixed(2);
-		}
+const getSum = () => {
+//Add your code here
+  let pricesEl = document.getElementsByClassName('price');
+  let sum = 0;
+  for (let i = 0; i < pricesEl.length; i++) {
+    let x = Number(pricesEl[i].innerText);
+    sum += x;
+  }
+    // console.log(`TotalSum: ${sum}`);
+    const emptyRow = document.createElement("tr");
+    emptyRow.innerHTML = `
+    <td></td><td>${sum}</td>
+    `
+    // document.getElementById('tab').appendChild(emptyRow);
+    document.getElementById('tab').innerHTML += `<tr id="ans">
+    <td></td><td>${sum}</td>
+        </tr>`
+};
+
+getSumBtn.addEventListener("click", getSum);
