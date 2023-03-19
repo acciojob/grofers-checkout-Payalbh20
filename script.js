df8ce3801bdead66ae9e2e9967d6968871923a47
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-19 10:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const calculateTotalBtn = document.getElementById('calculate-total-btn');
+		calculateTotalBtn.addEventListener('click', calculateTotal);
+
+		function calculateTotal() {
+			const prices = document.querySelectorAll('.price');
+			let totalPrice = 0;
+			prices.forEach(price => {
+				totalPrice += parseFloat(price.textContent);
+			});
+			const totalPriceCell = document.getElementById('ans');
+			totalPriceCell.textContent = totalPrice.toFixed(2);
+		}
 };
 
 getSumBtn.addEventListener("click", getSum);
